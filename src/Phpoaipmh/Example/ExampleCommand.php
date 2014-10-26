c35325f9f78e6211fbea39426550aa1e4f205859
Matthias Vandermaesen <matthias@colada.be>
2014-10-26 22:04:17 +0100

    changed: added options to the command to call each function separately.

--- src/Phpoaipmh/Example/ExampleCommand.php
+++ src/Phpoaipmh/Example/ExampleCommand.php
@@ -20,6 +20,7 @@ namespace Phpoaipmh\Example;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
 use Phpoaipmh\Example\Example;
@@ -35,35 +36,45 @@ class ExampleCommand extends Command {
 
     protected function configure() {
         $this->setName("phpoaipmh:example")
+            ->addOption('call', null, InputOption::VALUE_OPTIONAL, '', 'all')
             ->setDescription("Example implementation of the OAI-PMH library.");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $formatter = $this->getHelper('formatter');
+        $call = $input->getOption('call');
 
-        $data = $this->example->getBasicInformation();
-        $output->writeln("Basic information: ");
-        foreach ($data as $key => $value) {
-          $line = $formatter->formatSection($key, $value);
-          $output->writeln($line);
+        if ($call == 'basic' || $call == 'all') {
+            $data = $this->example->getBasicInformation();
+            $output->writeln("Basic information: ");
+            foreach ($data as $key => $value) {
+              $line = $formatter->formatSection($key, $value);
+              $output->writeln($line);
+            }
         }
 
-        $data = $this->example->getAvailableMetadataFormats();
-        $table = new Table($output);
-        $output->writeln("Metadata formats: ");
+        if ($call == 'formats' || $call == 'all') {
+            $data = $this->example->getAvailableMetadataFormats();
+            $table = new Table($output);
+            $output->writeln("Metadata formats: ");
 
-        $table->setHeaders($data['header']);
-        $table->setRows($data['rows']);
-        $table->render();
+            $table->setHeaders($data['header']);
+            $table->setRows($data['rows']);
+            $table->render();
+        }
 
-        $data = $this->example->getRecords();
-        $table = new Table($output);
-        $output->writeln("Ten records: ");
+        if ($call == 'records' || $call == 'all') {
+            $data = $this->example->getRecords();
+            $table = new Table($output);
+            $output->writeln("Ten records: ");
 
-        $table->setHeaders($data['header']);
-        $table->setRows($data['rows']);
-        $table->render();
+            $table->setHeaders($data['header']);
+            $table->setRows($data['rows']);
+            $table->render();
+        }
 
-        $data = $this->example->tryAnException();
+        if ($call == 'exception' || $call == 'all') {
+            $data = $this->example->tryAnException();
+        }
     }
 }
