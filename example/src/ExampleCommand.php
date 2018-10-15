61b0c322de9aaa4c67c63a03f5ba7fdadb44638c
Casey McLaughlin <caseyamcl@gmail.com>
2018-10-15 12:29:40 -0400

    Change LICENSE to LICENSE.md

--- example/src/ExampleCommand.php
+++ example/src/ExampleCommand.php
@@ -10,7 +10,7 @@
  * @author Casey McLaughlin <caseyamcl@gmail.com>
  * @author Matthias Vandermaesen <matthias.vandermaesen@gmail.com>
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright and license information, -please view the LICENSE.md
  * file that was distributed with this source code.
  *
  * ------------------------------------------------------------------
